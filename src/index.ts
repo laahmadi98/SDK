3514f756fb3ee9980f79391545193d39994e5978
laahmadi98 <la.ahmadi98@gmail.com>
2021-01-18 12:54:07 +0330

    add new object

--- src/index.ts
+++ src/index.ts
@@ -3,58 +3,58 @@
 const showcase = document.getElementById('showcase') as HTMLIFrameElement;
 const key = 'ppbuyxf83b8gf792q7afw8nqa';
 
-export {}
+export { }
 // augment window with the MP_SDK property
 declare global {
-  interface Window {
-    MP_SDK: any;
-  }
+    interface Window {
+        MP_SDK: any;
+    }
 }
 
 
 showcase.addEventListener('load', async function () {
 
-  
-  
-  
-        let sdk;
-        try {
-            sdk = await showcase.contentWindow.MP_SDK.connect(showcase, key, '3.6');
-        }
-        catch (e) {
-            console.error(e);
-            return;
-        }
-        console.log('%c  Hello Bundle SDK! ', 'background: #333333; color: #00dd00');
-        console.log(sdk);
-        //ADDING LIGHTS
-        const lights = await sdk.Scene.createNode();
-        var initial_light = {
-            enabled: true,
-            color: {
-                r: 1, g: 1, b: 1
-            },
-            intensity: 2,
-        };
-        lights.addComponent('mp.lights', initial_light);
-        lights.start();
-        //ADDING MODEL0---------------Main Floor (1)--------------------
-        const modelNode0 = await sdk.Scene.createNode();
-        // Store the fbx component since we will need to adjust it in the next step.
-        const objComponent0 = modelNode0.addComponent(sdk.Scene.Component.OBJ_LOADER, {
-            url: '/3D_models/cage-tables-by-tacchini/Coffe_Table_Cage_tables.obj',
-            materialUrl: '/3D_models/cage-tables-by-tacchini/Coffe_Table_Cage_tables.mtl',
-        });
-        //initial scaling 
-        objComponent0.inputs.localScale = {
-            x: 1,
-            y: 1,
-            z: 1
-        };
-        //initial positioning 
-        modelNode0.obj3D.position.set(4.18, -0.45, -0.45); // (x, z, -y ) 
-        modelNode0.obj3D.rotation.set(0, 0, 0); // (x, z, -y ) 
-        modelNode0.start();
+
+
+
+    let sdk;
+    try {
+        sdk = await showcase.contentWindow.MP_SDK.connect(showcase, key, '3.6');
+    }
+    catch (e) {
+        console.error(e);
+        return;
+    }
+    console.log('%c  Hello Bundle SDK! ', 'background: #333333; color: #00dd00');
+    console.log(sdk);
+    //ADDING LIGHTS
+    const lights = await sdk.Scene.createNode();
+    var initial_light = {
+        enabled: true,
+        color: {
+            r: 1, g: 1, b: 1
+        },
+        intensity: 2,
+    };
+    lights.addComponent('mp.lights', initial_light);
+    lights.start();
+    //ADDING MODEL0---------------Main Floor (1)--------------------
+    const modelNode0 = await sdk.Scene.createNode();
+    // Store the fbx component since we will need to adjust it in the next step.
+    const objComponent0 = modelNode0.addComponent(sdk.Scene.Component.OBJ_LOADER, {
+        url: '/3D_models/cage-tables-by-tacchini/Coffe_Table_Cage_tables.obj',
+        materialUrl: '/3D_models/cage-tables-by-tacchini/Coffe_Table_Cage_tables.mtl',
+    });
+    //initial scaling 
+    objComponent0.inputs.localScale = {
+        x: 1,
+        y: 1,
+        z: 1
+    };
+    //initial positioning 
+    modelNode0.obj3D.position.set(4.18, -0.45, -0.45); // (x, z, -y ) 
+    modelNode0.obj3D.rotation.set(0, 0, 0); // (x, z, -y ) 
+    modelNode0.start();
 
 
     //ADDING MODEL3---------------Main Floor (7)--------------------
@@ -75,40 +75,77 @@ showcase.addEventListener('load', async function () {
     modelNode3.obj3D.rotation.set(0, -4.71225, 0); // (x, z, -y ) 
     modelNode3.start();
 
-         //ADDING MODEL1---------------Main Floor (2)--------------------
-         const modelNode1 = await sdk.Scene.createNode();
-         // Store the fbx component since we will need to adjust it in the next step.
-         const objComponent1 = modelNode1.addComponent(sdk.Scene.Component.OBJ_LOADER, {
-             url: '/3D_models/Armchair With Pillows/Armchair with pillows.obj',
-             materialUrl: '/3D_models/Armchair With Pillows/Armchair with pillows.mtl',
-         });
-         //initial scaling 
-         objComponent1.inputs.localScale = {
-             x: 1,
-             y: 1,
-             z: 1
-         };
-         //initial positioning 
-         modelNode1.obj3D.position.set(-9.7, -0.45, -2.06); // (x, z, -y ) 
-         modelNode1.obj3D.rotation.set(0, 0, 0); // (x, z, -y ) 
-         modelNode1.start();
-         //ADDING MODEL2---------------Main Floor (3)--------------------
-         const modelNode2 = await sdk.Scene.createNode();
-         // Store the fbx component since we will need to adjust it in the next step.
-         const objComponent2 = modelNode2.addComponent(sdk.Scene.Component.OBJ_LOADER, {
-             url: '/3D_models/Armchair With Pillows/Armchair with pillows.obj',
-             materialUrl: '/3D_models/Armchair With Pillows/Armchair with pillows.mtl',
-         });
-         //initial scaling 
-         objComponent2.inputs.localScale = {
-             x: 1,
-             y: 1,
-             z: 1
-         };
-         //initial positioning 
-         modelNode2.obj3D.position.set(-9.7, -0.45, -0.81); // (x, z, -y ) 
-         modelNode2.obj3D.rotation.set(0, 0, 0); // (x, z, -y ) 
-         modelNode2.start();
-      
+    //ADDING MODEL1---------------Main Floor (2)--------------------
+    const modelNode1 = await sdk.Scene.createNode();
+    // Store the fbx component since we will need to adjust it in the next step.
+    const objComponent1 = modelNode1.addComponent(sdk.Scene.Component.OBJ_LOADER, {
+        url: '/3D_models/Armchair With Pillows/Armchair with pillows.obj',
+        materialUrl: '/3D_models/Armchair With Pillows/Armchair with pillows.mtl',
+    });
+    //initial scaling 
+    objComponent1.inputs.localScale = {
+        x: 1,
+        y: 1,
+        z: 1
+    };
+    //initial positioning 
+    modelNode1.obj3D.position.set(-9.7, -0.45, -2.06); // (x, z, -y ) 
+    modelNode1.obj3D.rotation.set(0, 0, 0); // (x, z, -y ) 
+    modelNode1.start();
+    //ADDING MODEL2---------------Main Floor (3)--------------------
+    const modelNode2 = await sdk.Scene.createNode();
+    // Store the fbx component since we will need to adjust it in the next step.
+    const objComponent2 = modelNode2.addComponent(sdk.Scene.Component.OBJ_LOADER, {
+        url: '/3D_models/Armchair With Pillows/Armchair with pillows.obj',
+        materialUrl: '/3D_models/Armchair With Pillows/Armchair with pillows.mtl',
+    });
+    //initial scaling 
+    objComponent2.inputs.localScale = {
+        x: 1,
+        y: 1,
+        z: 1
+    };
+    //initial positioning 
+    modelNode2.obj3D.position.set(-9.7, -0.45, -0.81); // (x, z, -y ) 
+    modelNode2.obj3D.rotation.set(0, 0, 0); // (x, z, -y ) 
+    modelNode2.start();
+
+
+    //ADDING MODEL4---------------Main Floor (1)--------------------
+    // const modelNode4 = await sdk.Scene.createNode();
+    // // Store the fbx component since we will need to adjust it in the next step.
+    // const objComponent4 = modelNode4.addComponent(sdk.Scene.Component.OBJ_LOADER, {
+    //     url: '/3D_models/17-potted_plant_obj/potted_plant_obj.obj',
+    //     materialUrl: '/3D_models/17-potted_plant_obj/potted_plant_obj.mtl',
+    // });
+    // //initial scaling 
+    // objComponent4.inputs.localScale = {
+    //     x: 0.03,
+    //     y: 0.03,
+    //     z: 0.03,
+    // };
+    // //initial positioning 
+    // modelNode4.obj3D.position.set(4.1, 0, -0.35); // (x, z, -y ) 
+    // modelNode4.obj3D.rotation.set(0, -1.1, 0); // (x, z, -y ) 
+    // modelNode4.start();
+
+
+    //ADDING MODEL4---------------Main Floor (1)--------------------
+    const modelNode5 = await sdk.Scene.createNode();
+    // Store the fbx component since we will need to adjust it in the next step.
+    const objComponent5 = modelNode5.addComponent(sdk.Scene.Component.FBX_LOADER, {
+        url: '/3D_models/uploads_files_629071_Gasteria_FBX/cactus_gasteria.fbx',
+    });
+    //initial scaling 
+    objComponent5.inputs.localScale = {
+        x: 0.03,
+        y: 0.03,
+        z: 0.03,
+    };
+    //initial positioning 
+    modelNode5.obj3D.position.set(4.1, 0, -0.35); // (x, z, -y ) 
+    modelNode5.obj3D.rotation.set(0, -1.1, 0); // (x, z, -y ) 
+    modelNode5.start();
+
 
 });
